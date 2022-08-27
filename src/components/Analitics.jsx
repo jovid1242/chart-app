ef5c2cb2234bff188afe3b8f2237c58f1e8f24bd
jovid1242 <jovid1242@gmail.com>
2022-08-27 20:58:17 +0500

    start project

---
+++ src/components/Analitics.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: "#c76009",
+      backgroundColor: "#c7600999",
+    },
+    {
+      fill: true,
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: "#09c7be",
+      backgroundColor: "#09c7be99",
+    },
+  ],
+};
+
+const Analitics = () => {
+  return (
+    <div className="chart-wrapper">
+      <Line options={options} data={data} />
+    </div>
+  );
+};
+
+export default Analitics;
